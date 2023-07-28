a55078657e60a036ff002400ae8cb026e090a75f
Jakub Hašek <28836407+kubahasek@users.noreply.github.com>
2023-07-28 10:24:02 +0200

    fix errors

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -20,8 +20,8 @@ function Counter({ value }: { value: number }) {
 }
 
 function Digit({ place, value }: { place: number; value: number }) {
-  let valueRoundedToPlace = Math.floor(value / place);
-  let animatedValue = useSpring(valueRoundedToPlace);
+  const valueRoundedToPlace = Math.floor(value / place);
+  const animatedValue = useSpring(valueRoundedToPlace);
 
   useEffect(() => {
     animatedValue.set(valueRoundedToPlace);
@@ -30,16 +30,16 @@ function Digit({ place, value }: { place: number; value: number }) {
   return (
     <div style={{ height }} className="relative w-[1ch] tabular-nums">
       {[...Array(10).keys()].map((i) => (
-        <Number key={i} mv={animatedValue} place={place} number={i} />
+        <Number key={i} mv={animatedValue} number={i} />
       ))}
     </div>
   );
 }
 
 function Number({ mv, number }: { mv: MotionValue; number: number }) {
-  let y = useTransform(mv, (latest) => {
-    let placeValue = latest % 10;
-    let offset = (10 + number - placeValue) % 10;
+  const y = useTransform(mv, (latest) => {
+    const placeValue = latest % 10;
+    const offset = (10 + number - placeValue) % 10;
 
     let memo = offset * height;
 
